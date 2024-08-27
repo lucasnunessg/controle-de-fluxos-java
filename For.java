0c59d9c03961f92070aec3a80165723bc6e008eb
lucasnunessg <lucasnunespacheco@gmail.com>
2024-08-27 11:24:26 -0300

    aprendendo mais e mais a cada dia no javinha, te amo javinha.

---
+++ For.java
@@ -0,0 +1,11 @@
+public class For {
+    public static void main(String[] args) {
+        int numero;
+        String log;
+
+        for (numero = 1; numero <= 10; numero ++) {
+            log = "O número atual é: " + numero + "\n";
+            System.out.println(log);
+        }
+    }
+}
