e7b6395d6048b4d05eb5674b7293326d73a2685f
lucasnunessg <lucasnunespacheco@gmail.com>
2024-08-28 12:13:19 -0300

    more

--- ReadData.java
+++ ReadData.java
@@ -3,9 +3,23 @@ import java.util.Scanner;
 public class ReadData {
     public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
-        System.out.print("Qual o seu nome? ");
-        String nome = scanner.next();
-        System.out.println("Boas vindas, " + nome + "!");
-        scanner.close();
+       String hello = "Olá!";
+       System.out.println("Diga olá: ");
+       String input = scanner.next();
+
+       scanner.close();
+
+       if (hello == input) {
+           System.out.println("isso não será impresso!");
+       }
+       if (hello != input) {
+           System.out.println("Isso será impresso!");
+       }
+       if (hello == hello) {
+           System.out.println("Isso também será impresso, é o mesmo objeto.");
+       }
+       if (hello.equals(input)) {
+           System.out.println("Isso vai ser impresso se for digitado 'olá'.");
+       }
     }
 }
